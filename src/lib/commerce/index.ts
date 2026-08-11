896ae40d7f2fbdf01c39ea86553a02ef15e61b35
Peter Pielaet-Strayer <peter.pielaetstrayer@gmail.com>
2026-08-11 10:58:25 -0400

    export digital fulfillment service

--- src/lib/commerce/index.ts
+++ src/lib/commerce/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './contracts';
 export * from './catalog';
-export * from './offer-service';
 export * from './legacy-product-adapter';
+export * from './offer-service';
+export * from './digital-fulfillment';
